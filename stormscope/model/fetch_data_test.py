91fcb178f0e90f46745ca2fdee80df10ff2b21eb
JianLi <44811760+jli-99@users.noreply.github.com>
2026-04-12 21:14:19 -0400

    test script of fetching local goes file

---
+++ stormscope/model/fetch_data_test.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+import numpy as np
+from earth2studio.data import GFS_FX, GOES, MRMS, fetch_data, DataArrayFile
+from earth2studio.models.px.stormscope import (
+    StormScopeBase,
+    StormScopeGOES,
+    StormScopeMRMS,
+)
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+goes_model_name = "6km_60min_natten_cos_zenith_input_eoe_v2"
+package = StormScopeBase.load_default_package()
+model = StormScopeGOES.load_model(
+    package=package,
+    conditioning_data_source=GFS_FX(),
+    model_name=goes_model_name,
+)
+
+variables = model.input_coords()["variable"]
+in_coords = model.input_coords()
+
+start_date = [np.datetime64(datetime(2023, 12, 5, 12, 00, 0))]
+GOES_INPUT_FILE = "goes_input.nc"
+goes_local = DataArrayFile(GOES_INPUT_FILE)
+x, x_coords = fetch_data(
+    goes_local,
+    time=start_date,
+    variable=np.array(variables),
+    lead_time=in_coords["lead_time"],
+    device=device,
+)
+print("GOES data fetched successfully.")
