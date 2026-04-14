eae927b7c37dcff203a56f7181f831e25116e5cc
JianLi <44811760+jli-99@users.noreply.github.com>
2026-04-13 22:30:38 -0400

    drop lead time in condition gfs

--- stormscope/model/simple_inference.py
+++ stormscope/model/simple_inference.py
@@ -16,7 +16,7 @@ NUM_STEPS = 2
 
 # Paths
 GOES_INPUT_FILE = "goes_input.nc"
-GFS_CONDITIONING_FILE = "gfs_conditioning.nc"
+GFS_CONDITIONING_FILE = "gfs_conditioning_nolead.nc"
 #PKG_PATH = "/stormscope/stormscope-goes-mrms"
 GOES_MODEL_NAME = "6km_60min_natten_cos_zenith_input_eoe_v2"
 OUTPUT_FILE = "./stormscope_forecast.nc"
