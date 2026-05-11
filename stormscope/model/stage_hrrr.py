a5eab20435f3d20e876950877464a2d03aad81a4
JianLi <44811760+jli-99@users.noreply.github.com>
2026-05-11 11:59:06 -0400

    update output path

--- stormscope/model/stage_hrrr.py
+++ stormscope/model/stage_hrrr.py
@@ -6,7 +6,7 @@ from earth2studio.data import HRRR, datasource_to_file
 
 time = np.array(["2024-09-26T12:00:00"], dtype="datetime64[ns]")
 ts_str = pd.to_datetime(time[0]).strftime("%Y%m%d_%H%M%S")
-out_file = f"hrrr_{ts_str}.nc"
+out_file = f"data/hrrr_{ts_str}.nc"
 # Use the variables your StormCast run will need.
 # Start with the exact same variables requested by the model/workflow.
 variables = np.array([
